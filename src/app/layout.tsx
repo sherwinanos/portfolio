24f6e20de46ac04ad51288c436e0a430d0451f63
Sherwin Anos <sherwin.anos@gmail.com>
2024-10-23 15:11:00 +0800

    Update: SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,28 +16,56 @@ const montserrat = Montserrat({
   variable: '--font-montserrat',
 });
 
+const seoName = "Sherwin Anos"
 const seoTitle = "Filipino Web Designer and Developer - Sherwin Anos"
 const seoDesc = "Get high-quality, affordable website design and development services from an experienced professional in the Philippines."
 const seoUrl = "https://www.sherwinanos.com/"
 
-export const metadata: Metadata = {
+export const metadata = {
   title: seoTitle,
   description: seoDesc,
   openGraph: {
     title: seoTitle,
     description: seoDesc,
     url: seoUrl,
-    siteName: "Sherwin Anos",
-    images: [{
-      url: seoUrl+'assets/og.webp',
-      width: 1200,
-      height: 630,
-      alt:"Sherwin Anos OG image"
-    }],
-    locale: "en_US",
-    type: "website"
-  }
+    siteName: seoName,
+    images: [
+      {
+        url: seoUrl+'assets/og.webp',
+        width: 1200,
+        height: 630,
+        alt: seoName+'OG image',
+      },
+    ],
+  },
+  alternates: {
+    canonical: seoUrl,
+  },
+  other: {
+    script: [
+      {
+        type: "application/ld+json",
+        children: JSON.stringify({
+          "@context": "https://schema.org",
+          "@type": "WebSite",
+          "name": seoName,
+          "url": seoUrl,
+          "author": {
+            "@type": "Person",
+            "name": seoName
+          },
+          "description": seoDesc,
+          "image": seoUrl+'assets/og.webp',
+          "publisher": {
+            "@type": "Organization",
+            "name": seoName
+          }
+        }),
+      },
+    ],
+  },
 };
+
 
 export default function RootLayout({
   children,
